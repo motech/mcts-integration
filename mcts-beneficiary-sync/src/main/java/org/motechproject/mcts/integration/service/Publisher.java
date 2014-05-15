7638dc7f05a0abeb0d559f35b6e36d3e0da41b4b
MohitGarg2905 <mohit.garg2905@gmail.com>
2014-05-15 20:28:20 +0530

    Added stub for hub Added ping-test to beneficiary-sync and publisher call back controllers.

--- mcts-beneficiary-sync/src/main/java/org/motechproject/mcts/integration/service/Publisher.java
+++ mcts-beneficiary-sync/src/main/java/org/motechproject/mcts/integration/service/Publisher.java
@@ -18,33 +18,71 @@ public class Publisher {
 	private PropertyReader propertyReader;
 	@Autowired
 	private RestTemplate restTemplate;
-	private final static String MODE="publish";
-	private final static Logger LOGGER = LoggerFactory.getLogger(Publisher.class);
+	private final static String MODE = "publish";
+	private final static Logger LOGGER = LoggerFactory
+			.getLogger(Publisher.class);
 	private static String URL;
 	private static String DATA;
-	
-	public void publish(String url, String data){
+
+	public void publish(String url, String data) {
 		DATA = data;
 		setUrl(url);
 		LOGGER.info("Syncing beneficiary data to MCTS.");
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-        HttpEntity httpEntity = new HttpEntity(httpHeaders);
-        ResponseEntity<String> response = restTemplate.postForEntity(getUrl(), httpEntity, String.class);
-        if (response != null)
-            LOGGER.info(String.format("Hub Notified Successfully. Response [StatusCode %s] : %s", response.getStatusCode(), response.getBody()));
+
+		ResponseEntity<String> response = null;
+		int maxRetryCount = propertyReader.getMaxNumberOfPublishRetryCount();
+		int retryCount = -1;
+		do {
+			LOGGER.info(String.format("Notify Hub for %s time at url %s.",
+					retryCount, URL));
+			response = notify(MediaType.APPLICATION_FORM_URLENCODED);;
+			retryCount++;
+			if (response.getStatusCode().value() / 100 == 2) {
+				LOGGER.info(String
+						.format("Hub Notified Successfully after %s retries. Response [StatusCode %s] : %s",
+								retryCount, response.getStatusCode(),
+								response.getBody()));
+				return;
+			}
+			try {
+				Thread.sleep(propertyReader.getHubRetryInterval());
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} while (retryCount == maxRetryCount
+				|| response.getStatusCode().value() / 100 == 2);
+
+		if (response.getStatusCode().value() / 100 == 2)
+			LOGGER.info(String
+					.format("Hub Notified Successfully after %s retries. Response [StatusCode %s] : %s",
+							retryCount, response.getStatusCode(),
+							response.getBody()));
+		else
+			LOGGER.error(String
+					.format("Notification to Hub failed. Response [StatusCode %s] : %s",
+							response.getStatusCode(), response.getBody()));
 	}
-	
-	private void setUrl(String url)
-	{
-		URL= String.format("%s%s%s%s%s",propertyReader.getHubBaseUrl(), "/hub?hub.mode=", MODE, "&hub.url=", url);
+
+	private ResponseEntity<String> notify(MediaType contentType) {
+		HttpHeaders httpHeaders = new HttpHeaders();
+		httpHeaders.setContentType(contentType);
+		HttpEntity httpEntity = new HttpEntity(httpHeaders);
+		ResponseEntity<String> response = restTemplate.postForEntity(getUrl(),
+				httpEntity, String.class);
+		return response;
 	}
-	
-	public String getUrl(){
+
+	private void setUrl(String url) {
+		URL = String.format("%s%s%s%s%s", propertyReader.getHubBaseUrl(),
+				"?hub.mode=", MODE, "&hub.url=", url);
+	}
+
+	public String getUrl() {
 		return URL;
 	}
-	
-	public String getData(){
+
+	public String getData() {
 		return this.DATA;
 	}
 }
