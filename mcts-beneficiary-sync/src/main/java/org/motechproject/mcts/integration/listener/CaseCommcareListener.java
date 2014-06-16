560258ace198706f540ea15645897bfa59254fc5
chandravanshi <naveenchandravanshi@gmail.com>
2014-06-16 14:42:19 +0530

    mcts is working as a bundle on 0.21.1
    Conflicts:
    	mcts-beneficiary-sync/pom.xml
    
    Conflicts:
    	mcts-beneficiary-sync/pom.xml

---
+++ mcts-beneficiary-sync/src/main/java/org/motechproject/mcts/integration/listener/CaseCommcareListener.java
@@ -0,0 +1,24 @@
+package org.motechproject.mcts.integration.listener;
+
+import org.motechproject.event.MotechEvent;
+import org.motechproject.event.listener.annotations.MotechListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.motechproject.commcare.events.constants.EventSubjects;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CaseCommcareListener {
+	
+	 private static final Logger logger = LoggerFactory.getLogger(CaseCommcareListener.class);
+	
+	    @Autowired
+	    public CaseCommcareListener() {
+	    }
+	    
+	    @MotechListener(subjects = EventSubjects.CASE_EVENT)
+	    public void handleEvent(MotechEvent event) {
+	    	
+	    }
+}
