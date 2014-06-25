9eedb9a4492a3e2e30a6d31b5468c3bcaf16367f
chandravanshi <naveenchandravanshi@gmail.com>
2014-06-25 21:08:14 +0530

    added comments and handled exception in all processor class

--- mcts-beneficiary-sync/src/main/java/org/motechproject/mcts/integration/processor/ClosedCaseProcessor.java
+++ mcts-beneficiary-sync/src/main/java/org/motechproject/mcts/integration/processor/ClosedCaseProcessor.java
@@ -3,33 +3,73 @@ package org.motechproject.mcts.integration.processor;
 import java.util.Map;
 
 import org.motechproject.commcare.events.CaseEvent;
+import org.motechproject.mcts.integration.exception.ApplicationErrors;
 import org.motechproject.mcts.integration.exception.BeneficiaryException;
 import org.motechproject.mcts.integration.hibernate.model.MctsPregnantMother;
 import org.motechproject.mcts.integration.service.CareDataService;
+import org.motechproject.mcts.integration.service.MCTSFormUpdateService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
+/**
+ * Class to handle closed cases sent from commcareHQ
+ * @author naveen
+ *
+ */
 @Component
 public class ClosedCaseProcessor implements CaseProcesssor {
 
 	@Autowired
 	CareDataService careDataService;
+	@Autowired
+	MCTSFormUpdateService mctsFormUpdateService;
 	private static final Logger logger = LoggerFactory.getLogger("ClosedCaseProcessor");
+	
+	/**
+	 * method to process closed case sent by commcareHQ
+	 * @param caseEvent: CaseEvent object sent by commcareHQ
+	 * @exception BeneficiaryException if mandatory fields are not there or database query fails
+	 */
 	@Override
 	public void process(CaseEvent caseEvent) throws BeneficiaryException {
 		
+		String hhNumber=null;
+		String familyNumber=null;
+		String mctsId = null;
 		logger.debug("Started sevice to handle closed case with caseId: "+caseEvent.getCaseId());
+		
+		//get fields received from commcareHQ
 		Map<String , String> fieldValues = caseEvent.getFieldValues();
-		String hhNumber = fieldValues.get("hhNumber");
-		String familyNumber = fieldValues.get("familyNumber");
-		String mctsId = fieldValues.get("mctsId");
+		
+		//Based on caseId get the corresponding objects from database 
 		MctsPregnantMother pregnantMotherCase = careDataService.findEntityByField(MctsPregnantMother.class, "mctsId", mctsId);
-		pregnantMotherCase.setHhNumber(hhNumber);
-		pregnantMotherCase.setFamilyNumber(familyNumber);
+		
+		//set hhNumber and familyNumber in MctsPregnantMother if not present throw an exception as they are mandatory fields
+		if(fieldValues.containsKey("hh_number") && fieldValues.containsKey("family_number")) {
+			hhNumber = fieldValues.get("hh_number");
+			familyNumber = fieldValues.get("family_number");
+			pregnantMotherCase.setHhNumber(hhNumber);
+			pregnantMotherCase.setFamilyNumber(familyNumber);
+		}
+		else {
+			throw new BeneficiaryException(ApplicationErrors.KEY_FIELD_DOESNOT_EXIST,"Either hh_number or family_number doesn't exist");
+		}
+		
+		//set mcts_id for motherCaseMctsUpdate and throws exception if there is mcts_id mismatch for received case and existing case
+		if(fieldValues.containsKey("mcts_id")) {
+			mctsId = fieldValues.get("mcts_id");
+			if(!mctsId.equals(pregnantMotherCase.getMctsId())) {
+				throw new BeneficiaryException(ApplicationErrors.FIELD_MISMATCH,"mcts_id of received persona case and existing persona case doesn't match for given caseId");
+			}
+		}
+		
+		//save mctsPregnantMother and motherCaseMctsUpdate in database
 		careDataService.saveOrUpdate(pregnantMotherCase);
-		//TODO call Aman function
+		
+		//Call to method to update caseid(from commcare case) for closed case in mcts pregnant mother 
+		mctsFormUpdateService.updateMctsPregnantMotherForm(pregnantMotherCase.getId());
+		logger.info("successfully handled the closed case with case Id: "+caseEvent.getCaseId()+" hhNumber"+hhNumber+" and family number"+familyNumber);
 	}
 
 }
--- mcts-beneficiary-sync/src/main/java/org/motechproject/mcts/integration/processor/MotherCaseProcessor.java
+++ mcts-beneficiary-sync/src/main/java/org/motechproject/mcts/integration/processor/MotherCaseProcessor.java
@@ -3,16 +3,22 @@ package org.motechproject.mcts.integration.processor;
 import java.util.Map;
 
 import org.motechproject.commcare.events.CaseEvent;
+import org.motechproject.mcts.integration.exception.ApplicationErrors;
 import org.motechproject.mcts.integration.exception.BeneficiaryException;
 import org.motechproject.mcts.integration.hibernate.model.MctsPregnantMother;
 import org.motechproject.mcts.integration.hibernate.model.MotherCase;
 import org.motechproject.mcts.integration.hibernate.model.MotherCaseMctsAuthorizedStatus;
+import org.motechproject.mcts.integration.hibernate.model.MotherCaseMctsUpdate;
 import org.motechproject.mcts.integration.service.CareDataService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
+/**
+ * Class to handle mother cases sent from commcareHQ  
+ * @author naveen
+ *
+ */
 @Component
 public class MotherCaseProcessor implements CaseProcesssor{
 
@@ -20,20 +26,65 @@ public class MotherCaseProcessor implements CaseProcesssor{
 	CareDataService careDataService;
 	private static final Logger logger = LoggerFactory.getLogger("MotherCaseProcessor");
 	
+	/**
+	 * method to process mother case sent by commcareHQ
+	 * @param caseEvent: CaseEvent object sent by commcareHQ
+	 * @exception throws BeneficiaryException if mandatory fields are not there or database query fails
+	 */
 	@Override
 	public void process(CaseEvent caseEvent) throws BeneficiaryException {
 		
 		logger.debug("Started processor to handle mother case with caseId" + caseEvent.getCaseId());
+		
+		String authorized = null;
+		String mctsId = null;
+		MotherCaseMctsAuthorizedStatus authorizeStatus = null;
+		
+		//get fields received from commcareHQ
 		Map<String , String> fieldValues = caseEvent.getFieldValues();
-		String authorized = fieldValues.get("authorized");
-		String fullMctsId = fieldValues.get("full_mcts_id");
-		MotherCaseMctsAuthorizedStatus authorizeStatus = careDataService.findEntityByField(MotherCaseMctsAuthorizedStatus.class, "name", authorized);
-		String mctsId = fieldValues.get("mcts_id");
-		MctsPregnantMother pregnantMotherCase = careDataService.findEntityByField(MctsPregnantMother.class, "mctsId", mctsId);
-		pregnantMotherCase.setMotherCaseMctsAuthorizedStatus(authorizeStatus);
+		
+		//Based on caseId get the corresponding objects from database  
+		MctsPregnantMother mctsPregnantMother = careDataService.findEntityByField(MctsPregnantMother.class, "caseId", caseEvent.getCaseId());
+		MotherCaseMctsUpdate motherCaseMctsUpdate = careDataService.findEntityByField(MotherCaseMctsUpdate.class, "caseId", caseEvent.getCaseId());
 		MotherCase motherCase = careDataService.findEntityByField(MotherCase.class,"caseId", caseEvent.getCaseId());
-		pregnantMotherCase.setMotherCase(motherCase);
-		careDataService.saveOrUpdate(pregnantMotherCase);
+		
+		if(motherCaseMctsUpdate == null) {
+			motherCaseMctsUpdate = new MotherCaseMctsUpdate();
+		}
+		
+		//authorize is mandatory field so if not present throw an exception
+		if(fieldValues.containsKey("authorized")) {
+		    authorized = fieldValues.get("authorized");
+		    authorizeStatus = careDataService.findEntityByField(MotherCaseMctsAuthorizedStatus.class, "name", authorized);
+		}
+		else {
+			logger.error("Stopping MotherCase Processor: couldn't get field value of authorize");
+			throw new BeneficiaryException(ApplicationErrors.KEY_FIELD_DOESNOT_EXIST,"authorized field doesn't exist");
+		}
+		
+		//set mcts_id for motherCaseMctsUpdate and throws exception if there is mcts_id mismatch for received case and existing case
+		if(fieldValues.containsKey("mcts_id")) {	
+		    mctsId = fieldValues.get("mcts_id");
+		    motherCaseMctsUpdate.setMctsId(mctsId);
+		    if(!mctsId.equals(mctsPregnantMother.getMctsId())) {
+				throw new BeneficiaryException(ApplicationErrors.FIELD_MISMATCH,"mcts_id of received mother case and existing mother case doesn't match for given caseId");
+			}
+		}
+		
+		//set fields of MotherCaseMctsUpdate
+		motherCaseMctsUpdate.setMctsPregnantMother(mctsPregnantMother);
+		motherCaseMctsUpdate.setMotherCase(motherCase);
+		motherCaseMctsUpdate.setMotherCaseMctsAuthorizedStatus(authorizeStatus);
+		
+		//set authorized status and MotherCase of MctsPregnantMother
+		mctsPregnantMother.setMotherCaseMctsAuthorizedStatus(authorizeStatus);
+		mctsPregnantMother.setMotherCase(motherCase);
+		
+		//save mctsPregnantMother and motherCaseMctsUpdate in database
+		careDataService.saveOrUpdate(mctsPregnantMother);
+		careDataService.saveOrUpdate(motherCaseMctsUpdate);
+		
+		logger.info("successfully handled the case with case Id: "+caseEvent.getCaseId());
 	
 	}
 
