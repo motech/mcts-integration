d3dd526e2930e7c989f3bf1d53228d942b0c79ea
chandravanshi <naveenchandravanshi@gmail.com>
2014-08-21 14:54:53 +0530

    Added http call in synctobatchlet

--- mcts-beneficiary-sync/src/main/java/org/motechproject/mcts/integration/handler/BeneficiarySyncToBatchlet.java
+++ mcts-beneficiary-sync/src/main/java/org/motechproject/mcts/integration/handler/BeneficiarySyncToBatchlet.java
@@ -1,40 +1,89 @@
 package org.motechproject.mcts.integration.handler;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.IOException;
+import java.io.InputStream;
 
 import javax.batch.api.Batchlet;
 
-import org.joda.time.DateTime;
-import org.motechproject.mcts.integration.model.Beneficiary;
-import org.motechproject.mcts.integration.service.MCTSBeneficiarySyncService;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpMethod;
+import org.apache.commons.httpclient.UsernamePasswordCredentials;
+import org.apache.commons.httpclient.auth.AuthScope;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.io.IOUtils;
+import org.motechproject.mcts.utils.PropertyReader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 
 public class BeneficiarySyncToBatchlet implements Batchlet {
 
     private static final Logger LOGGER = LoggerFactory
             .getLogger(BeneficiarySyncToBatchlet.class);
-    private List<Beneficiary> beneficiariesList = new ArrayList<Beneficiary>();
-    private DateTime startDate = new DateTime(2014, 01, 03, 12, 30, 30);
-    private DateTime endDate = new DateTime(2014, 02, 03, 12, 30, 30);
 
-    @Autowired
-    private MCTSBeneficiarySyncService mctsBeneficiarySyncService;
+    private HttpClient commonsHttpClient;
+  
+    private PropertyReader propertyReader;
 
-    @Override
+    public HttpClient getCommonsHttpClient() {
+        return commonsHttpClient;
+    }
+
+    public void setCommonsHttpClient(HttpClient commonsHttpClient) {
+        this.commonsHttpClient = commonsHttpClient;
+    }
+
+    public PropertyReader getPropertyReader() {
+        return propertyReader;
+    }
+
+    public void setPropertyReader(PropertyReader propertyReader) {
+        this.propertyReader = propertyReader;
+    }
+
     public String process() throws Exception {
-        mctsBeneficiarySyncService.syncBeneficiaryData(startDate, endDate);
-        LOGGER.info("beneficiaries list size"
-                + Integer.toString(beneficiariesList.size()));
-        return null;
+        String response = getRequest(propertyReader.getMctsSyncToLoginUrl());
+        LOGGER.info("Returned response from mcts 0.21.1",response);
+        return response;
+
     }
 
     @Override
     public void stop() throws Exception {
-        // TODO Auto-generated method stub
 
+    }
+
+    String getRequest(String requestUrl) {
+        HttpMethod getMethod = buildRequest(requestUrl);
+
+        try {
+            commonsHttpClient.executeMethod(getMethod);
+            InputStream responseBodyAsStream = getMethod
+                    .getResponseBodyAsStream();
+            return IOUtils.toString(responseBodyAsStream);
+        } catch (IOException e) {
+            LOGGER.warn("IOException while sending request to mcts 0.21.1: "
+                    + e.getMessage());
+        } finally {
+            getMethod.releaseConnection();
+        }
+
+        return null;
+    }
+    
+    private HttpMethod buildRequest(String url) {
+        HttpMethod requestMethod = new GetMethod(url);
+        authenticateMctsLogin();
+        return requestMethod;
+    }
+    
+    private void authenticateMctsLogin() {
+        commonsHttpClient.getParams().setAuthenticationPreemptive(true);
+        String userName = propertyReader.getMctsUserName();
+        String password = propertyReader.getMctsPassword();
+        LOGGER.debug("loginurl for mcts" + propertyReader.getMctsSyncToLoginUrl());
+        commonsHttpClient.getState().setCredentials(
+                new AuthScope(null, -1, null, null),
+                new UsernamePasswordCredentials(userName, password));
     }
 
 }
